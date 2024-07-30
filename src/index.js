2990856a8d3eb61a3047b5c6e7b33d7f6d4cbb13
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2024-07-30 16:25:16 +1000

    edited 3 JS modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 
-// console.log("testing index.js webpack");
-
-// const contentDiv = document.querySelector(".content");
-// const testH1 = document.createElement("h1");
-// testH1.textContent = "TESTING DOM MANIPULATION";
-// contentDiv.appendChild(testH1);
-
-
-
 import { createToDo } from './create-to-do.js' ;
 import { blankProjectLoad } from './blank-project.js' ;
 import { initialdomManip } from './initial-dom-manip.js'
@@ -21,3 +12,7 @@ initialdomManip();
 // TODO: click event module HERE for todo and project creation
 
 // Call create-to-do.js module file and apply some objects/properties
+const myTodo = createToDo("Grocery Run", "Go get groceries", "15/6/2024", "Low", "Meat, Eggs, Milk");
+const myTodo2 = createToDo("Homework", "Do Odin Project work", "20/6/2024", "Medium", "Read Module, do Todo project");
+console.log("Show me properties on myTodo from index.js.....", myTodo);
+console.log("Show me properties on myTodo from index.js.....", myTodo2);

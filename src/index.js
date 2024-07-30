5921a1d3f08353bc699864cb81c164a8f8ce5b21
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2024-07-30 13:59:53 +1000

    Created 3 JS modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-console.log("testing index.js webpack");
 
-const contentDiv = document.querySelector(".content");
-const testH1 = document.createElement("h1");
-testH1.textContent = "TESTING DOM MANIPULATION";
-contentDiv.appendChild(testH1);
+// console.log("testing index.js webpack");
+
+// const contentDiv = document.querySelector(".content");
+// const testH1 = document.createElement("h1");
+// testH1.textContent = "TESTING DOM MANIPULATION";
+// contentDiv.appendChild(testH1);
+
+
+
+import { createToDo } from './create-to-do.js' ;
+import { blankProjectLoad } from './blank-project.js' ;
+import { initialdomManip } from './initial-dom-manip.js'
+
+// Call blankProjectLoad on first land
+blankProjectLoad();
+
+//Call DOM Manipulation module to control UI
+initialdomManip();
+
+// TODO: click event module HERE for todo and project creation
+
+// Call create-to-do.js module file and apply some objects/properties

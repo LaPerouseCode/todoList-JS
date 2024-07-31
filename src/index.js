14fbc726251d87a0a9830516805e0d7d321c5367
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2024-07-31 15:28:53 +1000

    edited 3 JS modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 
 import { createToDo } from './create-to-do.js' ;
-import { blankProjectLoad } from './blank-project.js' ;
-import { initialdomManip } from './initial-dom-manip.js'
+import { blankProjectLoad } from './blank-project-load.js' ;
+import { displayDefaultProject, displayTheForm, addItemToChecklist, clearForm } from './dom-manip.js';
+import 'style.css'
+
 
 // Call blankProjectLoad on first land
 blankProjectLoad();
@@ -9,7 +11,27 @@ blankProjectLoad();
 //Call DOM Manipulation module to control UI
 initialdomManip();
 
-// TODO: click event module HERE for todo and project creation
+//Click events module 
+let clickEventsModule = (function(){
+
+    //Click event for displaying the form
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
+
+    //Click event for adding an item to the checklist on the form
+    const addToChecklist = document.querySelector(".add-to-checklist");
+    addToChecklist.addEventListener("click", addItemToChecklist);
+
+    // Click event to clear the form
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm);
+
+})
+
+
+
+
+
 
 // Call create-to-do.js module file and apply some objects/properties
 const myTodo = createToDo("Grocery Run", "Go get groceries", "15/6/2024", "Low", "Meat, Eggs, Milk");
